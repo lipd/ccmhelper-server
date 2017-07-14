2bdfc7230a43bc475069efe31950c64d42490222
lipd <lipeidao@gmail.com>
2017-07-14 19:44:34 +0800

    test: write a test comment for Message Schema

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const Message = require('./models/Message')
 
+// this will create ccmhelper db
 const mongoPath = 'mongodb://localhost:27017/ccmhelper'
 
 mongoose.connect(mongoPath)
@@ -10,7 +12,22 @@ mongoose.connect(mongoPath)
 // const db = mongoose.connection
 // db.on('error', console.log)
 // db.once('openUri', () => {
-//   console.log("success")
+//   console.log('success')
+// })
+
+// ** Test Message Schema **
+// const db = mongoose.connection
+// db.on('error', console.log)
+// db.once('open', () => {
+//   let message = new Message({
+//     title: '这是标题',
+//     author: '这是作者',
+//     content: '这是内容'
+//   })
+//   message.save((err) => {
+//     if (err) console.log(err)
+//   })
+//   console.log('success')
 // })
 
 app.get('/messages', (req, res) => {
