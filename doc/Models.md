e451f8d72c9ba75203a02cb073d8f4c0c39c2b03
lipd <lipeidao@gmail.com>
2017-07-14 19:24:28 +0800

    feat: create message schema

---
+++ doc/Models.md
@@ -0,0 +1,12 @@
+# Models
+
+## Message
+
+```js
+{
+  time: Date,
+  title: String,
+  author: String,
+  content: String
+}
+```
---
+++ models/Message.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const MessageSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    content: { type: String, required: true }
+  },
+  { timestamps: true }
+)
+
+// This will create a collection called messages
+module.exports = mongoose.model('Message', MessageSchema)
