1a5c7ddc836452d78b01ac05cd29e2ab1f65c677
lipd <lipeidao@gmail.com>
2017-07-19 22:27:53 +0800

    comment: delete test code comment

--- app.js
+++ app.js
@@ -22,21 +22,6 @@ mongoose.connect(mongoPath)
 //   console.log('success')
 // })
 
-// ** Test Message Schema **
-// const db = mongoose.connection
-// db.on('error', console.log)
-// db.once('open', () => {
-//   let message = new Message({
-//     title: '这是标题',
-//     author: '这是作者',
-//     content: '这是内容'
-//   })
-//   message.save((err) => {
-//     if (err) console.log(err)
-//   })
-//   console.log('success')
-// })
-
 app.get('/messages', (req, res) => {
   Message.find().sort({'createdAt': -1}).exec((err, messages) => {
     if (err) {
