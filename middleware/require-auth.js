4a86fa80aa0317dadbbfebddf19e45e1929f582a
lipd <lipeidao@gmail.com>
2017-07-24 23:46:20 +0800

    feat: create require-auth.js middleware

---
+++ middleware/require-auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = function (req, res, next) {
+  let token = req.headers.authorization;
+  if(token) {
+    jwt.verify(token, config.jwtSecret, (err, decoded) => {
+      if(err) {
+        if(err.name === 'TokenExpiredError') {
+          return res.status(401).json({ error: 'Token Expired' });
+        } else {
+          return res.status(401).json({ error: 'Authentication Filure'});
+        }
+      } else {
+        if(decoded.openid) {
+          req.openid = decoded.openid;
+          next();
+        } else {
+          res.status(401).json({ error: 'Authentication Filure'});
+        }
+      }
+    });
+  } else {
+    return res.status(403).json({
+      error: 'No Token'
+    });
+  }
+}
