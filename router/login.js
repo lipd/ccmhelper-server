fb0b9204185fd144bd77ace49bc2a8003207223c
lipd <lipeidao@gmail.com>
2017-07-25 09:08:49 +0800

    feat: check user from database

--- router/login.js
+++ router/login.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const axios = require('axios')
 const jwt = require('jsonwebtoken')
-const Message = require('../models/Message')
+const User = require('../models/User')
 const config = require('../config')
 
 const generateToken = function (user) {
@@ -15,10 +15,25 @@ router.post('/login', (req, res) => {
   const queryString = `appid=${config.appId}&secret=${config.appSecret}&js_code=${req.body.code}&grant_type=authorization_code`
   const wxAPI = `https://api.weixin.qq.com/sns/jscode2session?${queryString}`
   axios.get(wxAPI)
-    .then(response => {
-      console.log(response.data)
-      res.json({
-        token: generateToken({openId: response.data.openId})
+    .then(wxRes => {
+      User.findOne({openId: wxRes.data.openid}, (err, user) => {
+        if (user) {
+          // TODO: check user's avatar everytime
+          return res.json({
+            token: generateToken({openId: wxRes.data.openid})
+          })
+        } else {
+          const userData = {
+            openId: wxRes.data.openid,
+            avatarUrl: req.body.avatarUrl
+          }
+          const user = new User(userData)
+          user.openId = wxRes.data.openid
+          user.save()
+          return res.json({
+            token: generateToken({openId: wxRes.data.openid}),
+          })
+        }
       })
     })
     .catch(error => {
